5281e38171d5ead15c3adcb84a4dcc112e305250
Lucas Newlands <58925749+newlandslucas@users.noreply.github.com>
2022-01-18 01:58:23 -0300

    feature/ adjustments

--- src/index.js
+++ src/index.js
@@ -1,50 +1,35 @@
 const os = require('os');
 
 function mySystemStatus() {
-    
     setInterval(() => {
 
         console.clear()
-        console.log("====== PC STATUS ====== \n\n")
-        
-        const system = os.type();
-        const architecture = os.arch();
+        console.log("====== PC STATUS ====== \n")
 
-
-        const type = {
-            Operation_System: `${system}`,
-            Artchitecture: `${architecture}`
-        }
-
-        console.table(type)
-
-    //Memory RAM
-
-        const { freemem, totalmem } = os
-
-        const total = parseInt(totalmem() / 1024 / 1024);
-        const mem = parseInt(freemem() / 1024 / 1024);
-        const percents = parseInt(((mem / total)) * 100);
-        const usageMemory = (100 -percents)
-
+    
+        const { freemem, totalmem } = os;
+    
+        const total = parseInt(totalmem() / 1024 / 1024)
+        const mem = parseInt(freemem() / 1024 / 1024)
+        const percentage_free = parseInt((mem / total) * 100)
+        const percentage_usage = (100 - percentage_free)
+    
         const stats = {
-            total:`${total} MB`,
-            free:`${mem} MB`,
-            percetage_usage:`${usageMemory}%`
+            total: `${total} MB`,
+            free: `${mem}MB`,
+            percentage_usage: `${percentage_usage}%`
         }
-
-        if(usageMemory <= 50) {
-                console.table(stats),
-                console.log('\nSistema OK!')
+    
+        if(percentage_usage <= 50) {
+            console.table(stats),
+            console.log('\nSistema OK!')
         } else {
                 console.table(stats),
-                console.log(`\nSistema usando ${stats.percetage_usage}, da memória ram disponível 🔥`)
+                console.log(`\nSistema usando ${stats.percentage_usage},\nsomente ${percentage_free}% do sistema livre.`)
         }
-
-        console.log('\n\nBy: Lucas Newlands          Version: 0.1.0')
+    
+        console.log('\n\nBy: Lucas Newlands')
     }, 1000)
 }
 
 console.log(mySystemStatus())
-
-
